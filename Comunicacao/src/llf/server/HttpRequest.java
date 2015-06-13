151d5b5755784b145aa8c46329a4b2357c2687ee
Lucas Lucena Falbo <95.lucas@gmail.com>
2015-06-13 11:59:53 -0300

    Servidor Implementado conforme especificação

--- Comunicacao/src/llf/server/HttpRequest.java
+++ Comunicacao/src/llf/server/HttpRequest.java
@@ -2,6 +2,7 @@ package llf.server;
 
 import java.io.*;
 import java.net.*;
+import java.util.StringTokenizer;
 
 public final class HttpRequest implements Runnable {
 	
@@ -30,7 +31,7 @@ public final class HttpRequest implements Runnable {
 	private void processRequest() throws Exception {
 		
 		InputStream input = socket.getInputStream();
-		OutputStream out = socket.getOutputStream();
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 		
 		BufferedReader read = new BufferedReader(new InputStreamReader(input));
 		String request = read.readLine();
@@ -38,11 +39,50 @@ public final class HttpRequest implements Runnable {
 		System.out.println();
 		System.out.println(request);
 		
-		String headerLine = null;
-		while ((headerLine = read.readLine()).length() != 0) {
-			System.out.println(headerLine);
-			
+		//Pegar o nome do arquivo
+		StringTokenizer tokens = new StringTokenizer(request);
+		tokens.nextToken();  
+		String arquivo = tokens.nextToken();
+		arquivo = "." + arquivo;
+		
+		//Tratando caso nao haja arquivo nomeado
+		FileInputStream fis = null;
+		boolean arquivoExiste = true;
+		try {
+			fis = new FileInputStream(arquivo);
+		} catch (FileNotFoundException e) {
+			arquivoExiste = false;
 		}
+		
+		// Mensagem de resposta
+		String statusLine = null;
+		String contentTypeLine = null;
+		String entityBody = null;
+		
+		if (arquivoExiste) {
+			statusLine = "HTTP-Version 1.0 Status-Code 200 SUCCESS";
+			contentTypeLine = "Content-type: " +   contentType(arquivo) + CRLF;
+
+		}else {
+			statusLine = "HTTP-Version 1.0 Status-Code 404 NOT FOUND";
+			contentTypeLine = "text/html";
+			entityBody = "<HTML>" + 
+				"<HEAD><TITLE>Not Found</TITLE></HEAD>" +
+				"<BODY>Not Found</BODY></HTML>";
+		}
+		
+		//Criando o header
+		out.writeBytes(statusLine);
+		out.writeBytes(contentTypeLine);
+		out.writeBytes(CRLF);
+
+		if(arquivoExiste){
+			sendBytes(fis, out);
+			fis.close();
+		}else {
+			out.writeBytes(entityBody);
+		}
+		
 		
 		out.close();
 		read.close();
@@ -51,6 +91,33 @@ public final class HttpRequest implements Runnable {
 
 
 		
+	}
+
+	private static void sendBytes(FileInputStream fis, OutputStream os)  throws Exception {
+			   // Construct a 1K buffer to hold bytes on their way to the socket.
+			   byte[] buffer = new byte[1024];
+			   int bytes = 0;
+
+			   // Copy requested file into the socket's output stream.
+			   while((bytes = fis.read(buffer)) != -1 ) {
+			      os.write(buffer, 0, bytes);
+			   }
+			}
+
+	private static String contentType(String fileName){
+		if(fileName.endsWith(".htm") || fileName.endsWith(".html")) {
+			return "text/html";
+		}
+		if(fileName.endsWith(".txt")) {
+			return "text/plain";
+		}
+		if(fileName.endsWith(".jpg")) {
+			return "image/jpeg";
+		}
+		if(fileName.endsWith(".css")) {
+			return "text/css";
+		}
+		return "application/octet-stream";
 	}
 
 
